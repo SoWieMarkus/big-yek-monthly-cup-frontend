f6c59b5ccf1ab0e97056716aac8dc64062d98c63
SoWieMarkus <wie.markus@web.de>
2024-08-04 11:18:55 +0200

    added privacy policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,41 +5,47 @@ import { AdminCupComponent } from "./pages/admin/components/admin-cup/admin-cup.
 import { AdminDashboardComponent } from "./pages/admin/components/admin-dashboard/admin-dashboard.component";
 import { LoginComponent } from "./pages/login/login.component";
 import { StandingsComponent } from "./pages/standings/standings.component";
+import { PrivacyPolicyComponent } from "./pages/privacy-policy/privacy-policy.component";
 
 export const routes: Routes = [
-	{
-		path: "admin",
-		component: AdminComponent,
-		title: "BYMC: Admin Panel",
-		canActivate: [AuthGuard],
-		children: [
-			{
-				path: "dashboard",
-				component: AdminDashboardComponent,
-			},
-			{
-				path: "cup/:id",
-				component: AdminCupComponent,
-			},
-		],
-	},
-	{
-		path: "standings",
-		component: StandingsComponent,
-	},
-	{
-		path: "login",
-		component: LoginComponent,
-		title: "BYMC: Login",
-	},
-	{
-		path: "",
-		redirectTo: "/standings",
-		pathMatch: "full",
-	},
-	{
-		path: "**",
-		redirectTo: "/standings",
-		pathMatch: "full",
-	},
+  {
+    path: "admin",
+    component: AdminComponent,
+    title: "BYMC: Admin Panel",
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: "dashboard",
+        component: AdminDashboardComponent,
+      },
+      {
+        path: "cup/:id",
+        component: AdminCupComponent,
+      },
+    ],
+  },
+  {
+    path: "standings",
+    component: StandingsComponent,
+  },
+  {
+    path: "login",
+    component: LoginComponent,
+    title: "BYMC: Login",
+  },
+  {
+    path: "privacy-policy",
+    component: PrivacyPolicyComponent,
+    title: "BYMC: Privacy Policy"
+  },
+  {
+    path: "",
+    redirectTo: "/standings",
+    pathMatch: "full",
+  },
+  {
+    path: "**",
+    redirectTo: "/standings",
+    pathMatch: "full",
+  },
 ];
